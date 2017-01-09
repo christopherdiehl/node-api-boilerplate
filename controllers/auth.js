5176e0b1e76523f90fe24ae28d2ec0f95b8b2b9d
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2017-01-08 22:03:39 -0500

    Switched over completely from passport to jwt-token. Makes more sense for a rest api in my opinion

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 // Load required packages
-var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
-var User = require('../models/user');
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const config = require('../config');
 
 exports.authenticate = function(req, res) {
   username = req.body.username;
@@ -20,32 +23,41 @@ exports.authenticate = function(req, res) {
       if (!isMatch) { return callback(null, false); }
       // Success
       //this is where I need to return an actual token
-      return res.json({"message":"Success bruh!"})
+      const token = jwt.sign({
+        username: user.username
+      }, config.jwtSecret);
+      return res.json({
+        token,
+        username: user.username
+      });
     });
   });
 
 }
 
-//this is where I need to check the token
-passport.use(new BasicStrategy(
-  function(username, password, callback) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return callback(err); }
+exports.isAuthenticated = expressJwt({ secret: config.jwtSecret });
 
-      // No user found with that username
-      if (!user) { return callback(null, false); }
-
-      // Make sure the password is correct
-      user.verifyPassword(password, function(err, isMatch) {
-        if (err) { return callback(err); }
-
-        // Password did not match
-        if (!isMatch) { return callback(null, false); }
-        // Success
-        return callback(null, user);
-      });
-    });
-  }
-));
-
-exports.isAuthenticated = passport.authenticate('basic', { session : false });
+//
+// //this is where I need to check the token
+// passport.use(new BasicStrategy(
+//   function(username, password, callback) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return callback(err); }
+//
+//       // No user found with that username
+//       if (!user) { return callback(null, false); }
+//
+//       // Make sure the password is correct
+//       user.verifyPassword(password, function(err, isMatch) {
+//         if (err) { return callback(err); }
+//
+//         // Password did not match
+//         if (!isMatch) { return callback(null, false); }
+//         // Success
+//         return callback(null, user);
+//       });
+//     });
+//   }
+// ));
+//
+// exports.isAuthenticated = passport.authenticate('basic', { session : false });
