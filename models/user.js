ccb9d3540660e6ba36263ac6ef74d9f2fc01cd6f
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2017-02-06 15:16:18 -0500

    Removed istanbul, added more tests for the reset password

--- models/user.js
+++ models/user.js
@@ -45,4 +45,9 @@ UserSchema.methods.verifyPassword = function(password, cb) ***REMOVED***
   ***REMOVED***);
 ***REMOVED***;
 
+UserSchema.statics.generateNewPassword = function(cb)***REMOVED***
+  let new_password = (Math.random().toString(36)+'00000000000000000').slice(2, 14);
+  return new_password;
+***REMOVED***
+
 module.exports = mongoose.model('User',UserSchema);
