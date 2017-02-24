162831f00f8bb246e94074073699767d74f43b2d
christopherdiehl <csd92@scarletmail.rutgers.edu>
2017-02-24 12:15:18 -0500

    Fixed auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,32 +8,32 @@ const config = require('../config/config')[env];
 exports.authenticate = function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
-  if(password == null || username == null){
-    res.sendStatus(400);
-    return;
-  }
-  User.findOne({
-    where: { username: username }
-  }).then((user) => {
-    // Make sure the password is correct
-    if (user.verifyPassword(password)) {
-      const token = jwt.sign({
-        username: user.username,
-        id: user.id
-      }, config.jwtSecret);
-      res.json({
-        success: true,
-        token: token,
-        username: user.username
-      });
-    } else {
-      //incorrect password
+  if(password && username) {
+    User.findOne({
+      where: { username: username }
+    }).then((user) => {
+      // Make sure the password is correct
+      if (user.verifyPassword(password)) {
+        const token = jwt.sign({
+          username: user.username,
+          id: user.id
+        }, config.jwtSecret);
+        res.json({
+          success: true,
+          token: token,
+          username: user.username
+        });
+      } else {
+        //incorrect password
+        res.sendStatus(404);
+      }
+    }).catch((error) => {
+      console.error(error);
       res.sendStatus(404);
-    }
-  }).catch((error) => {
-    console.error(error);
-    res.sendStatus(404);
-  });
+    });
+  } else {
+    res.sendStatus(400);
+  }
 }
 
 exports.isAuthenticated = expressJwt({ secret: config.jwtSecret });
