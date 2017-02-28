be2586e18d2d6d841151d8e5ec93602d9bce1ea1
christopherdiehl <csd92@scarletmail.rutgers.edu>
2017-02-28 15:48:53 -0500

    Added cors

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userController = require('./controllers/user');
 const authController = require('./controllers/auth');
-
+const cors = require('cors');
 
 module.exports = (port) => {
   var app = express();
+  app.use(cors());
   var port = port || 8080;
   var router = express.Router();
   app.use(bodyParser.urlencoded({extended : true}));
